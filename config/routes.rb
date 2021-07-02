28f85ee9c43b2b03c8d72032a5a325e5e4ed7aa0
whakapapa-HTML <uriah.heep0810@gmaiil.com>
2021-07-02 09:34:47 +0000

    Add いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books do
-    resources :book_comments, only: [:create, :destroy]
-    end  
+   resources :books do
+    resource :favorites, only: [:create,:destroy] do
+      collection do
+        post 'index_create'
+        delete 'index_destroy'
+        
+      end
+    end
+     resources :book_comments, only: [:create, :destroy]
+   end  
   root 'homes#top'
   get 'homes/about' => 'homes#about'
   
