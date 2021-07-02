5116e919f37b17abf7dc98a09d703de8fb93662e
whakapapa-HTML <uriah.heep0810@gmaiil.com>
2021-07-02 02:56:27 +0000

    Finish 課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,35 @@
 class BooksController < ApplicationController
+   before_action :authenticate_user! 
+   before_action :ensure_correct_user, only:[:edit, :update, :destroy]
+
 
   def show
+    @newbook = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
+      @book = Book.new(book_params)
+      @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      @user = User.find(current_user.id)
+      render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-
-
 
   def update
     @book = Book.find(params[:id])
@@ -34,16 +40,23 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
-  def book_params
-    params.require(:book).permit(:title)
-  end
+    def book_params
+      params.require(:book).permit(:title,:body)
+    end
+    
+    def ensure_correct_user
+      @book = Book.find(params[:id])
+      unless @book.user == current_user
+        redirect_to books_path
+      end
+    end
 
 end
