b41542f66c3d5ce2d2a0c94758246c218393c4d9
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-07-02 13:19:31 +0000

    Add Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                      foreign_key: "follower_id",
+                      dependent: :destroy
+  has_many :following, through: :active_relationships,source: :followed
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
