0d6f5e8971a8b2d7f3eba8542d4cdb7bc6a9a19e
whakapapa-HTML <uriah.heep0810@gmaiil.com>
2021-07-02 07:53:14 +0000

    Add Delete button

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-  
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    end  
   root 'homes#top'
   get 'homes/about' => 'homes#about'
   
